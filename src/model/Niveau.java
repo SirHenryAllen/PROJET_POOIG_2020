9ceda65cce5e270068dc42612296c77df50153fe
Matthieu Le Franc <matt.lefranc@gmail.com>
2020-11-29 00:14:43 +0100

    selection blocks destructible et suppression

--- src/model/Niveau.java
+++ src/model/Niveau.java
@@ -19,17 +19,17 @@ public class Niveau extends Groupe {
 				this._plateau.setBlock(0, b, new BlockFixe('a'));
 			}
 			for (int i = 1 ; i < 3 ; i++) {
-				for (int j = 1 ; j < 10 ; j++) {
+				for (int j = 10 ; j < 21 ; j++) {
 					this._plateau.setBlock(j, i, new BlockDestructible('a'));
 				}
 			}
-			for (int i = 4 ; i < 7 ; i++) {
-				for (int j = 1 ; j < 10 ; j++) {
+			for (int i = 4 ; i < 8 ; i++) {
+				for (int j = 10 ; j < 21 ; j++) {
 					this._plateau.setBlock(j, i, new BlockDestructible('b'));
 				}
 			}
-			for (int i = 8 ; i < 10 ; i++) {
-				for (int j = 1 ; j < 10 ; j++) {
+			for (int i = 9 ; i < 11 ; i++) {
+				for (int j = 10 ; j < 21 ; j++) {
 					this._plateau.setBlock(j, i, new BlockDestructible('c'));
 				}
 			}
