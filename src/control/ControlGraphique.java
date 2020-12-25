48a0764cc46a2cbbb1e221b9d01ad4b9d129f705
Matthieu Le Franc <matt.lefranc@gmail.com>
2020-12-25 20:00:08 +0100

    Ménage

---
+++ src/control/ControlGraphique.java
@@ -0,0 +1,70 @@
+package control;
+
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+import model.Plateau;
+import view.AffichageGraphique;
+
+import java.awt.Color;
+
+import java.awt.event.MouseEvent;
+
+public class ControlGraphique {
+    
+    public ControlGraphique() {}
+
+    public static class Carre extends JPanel implements MouseInputListener {
+        private static final long serialVersionUID = 1L;
+        Plateau p;
+        AffichageGraphique affichage;
+        
+        public Carre(Color color, Plateau p, AffichageGraphique a) {
+            this.setBounds(100, 200, 50, 50);
+            this.p = p;
+            this.affichage = a;
+            this.setBackground(color);
+            this.addMouseListener(this);
+            this.addMouseMotionListener(this);
+        }
+
+        // Déclenche l'événement lorsqu'un clique est détecté dans une zone particulière
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            this.p.supprimer(this.affichage.getPos(this)[0] + 11, this.affichage.getPos(this)[1] + 1, new boolean[p.getHauteur()][p.getLargeur()]);
+            this.p.actualiser();
+            this.affichage.reload(p);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //TODO
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            //TODO
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            //TODO
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            //TODO
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+    }    
+}
